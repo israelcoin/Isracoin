6893ff09ea0e2d7d3940b7008b2400c6b9d22061
Alon Muroch <alonmuroch@gmail.com>
2014-05-31 19:02:12 +0200

    scriptsig max size to 1650

--- src/main.cpp
+++ src/main.cpp
@@ -435,10 +435,17 @@ bool IsStandardTx(const CTransaction& tx, string& reason)
 
     BOOST_FOREACH(const CTxIn& txin, tx.vin)
     {
-        // Biggest 'standard' txin is a 3-signature 3-of-3 CHECKMULTISIG
-        // pay-to-script-hash, which is 3 ~80-byte signatures, 3
-        // ~65-byte public keys, plus a few script ops.
-        if (txin.scriptSig.size() > 500) {
+    	// UPDATE FROM BITCOIN REFERENCE CLIENT :
+    	//	See (https://github.com/bitcoin/bitcoin/commit/4d79098ad548874ca6e4c09d873fc2f481e6edb4#diff-7ec3c68a81efff79b6ca22ac1f1eabbaR494)
+    	// -------------------------------------------------------------------
+    	// Biggest 'standard' txin is a 15-of-15 P2SH multisig with compressed
+    	// keys. (remember the 520 byte limit on redeemScript size) That works
+    	// out to a (15*(33+1))+3=513 byte redeemScript, 513+1+15*(73+1)=1624
+    	// bytes of scriptSig, which we round off to 1650 bytes for some minor
+    	// future-proofing. That's also enough to spend a 20-of-20
+    	// CHECKMULTISIG scriptPubKey, though such a scriptPubKey is not
+    	// considered standard)
+        if (txin.scriptSig.size() > 1650) {
             reason = "scriptsig-size";
             return false;
         }
